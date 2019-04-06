f262c68251dfc5b35acc55a70910ddab7213a5f9
Apetri George <apetrigeorge@yahoo.com>
2019-04-06 14:19:44 +0300

    added client

--- booking-accommodation-core/src/main/java/ro/sda/bookingaccommodation/core/entity/Client.java
+++ booking-accommodation-core/src/main/java/ro/sda/bookingaccommodation/core/entity/Client.java
@@ -1,7 +1,21 @@
 package ro.sda.bookingaccommodation.core.entity;
 
-public class Client {
+import ro.sda.bookingaccommodation.core.base.BaseEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "clients", schema = "booking-accommodation")
+public class Client extends BaseEntity {
 
+    @Column(name = "name", length = 40, nullable = false)
+    private String name;
+
+    @Column(name = "email", length = 40, nullable = false)
+    private String email;
+
+    @Column(name = "telephone", length = 40, nullable = false)
+    private String telephone;
 }
