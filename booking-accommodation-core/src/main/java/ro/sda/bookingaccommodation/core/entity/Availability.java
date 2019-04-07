78300211dd0a77b9114aa46ff0fa59f953e95c68
Apetri George <apetrigeorge@yahoo.com>
2019-04-07 10:59:25 +0300

    added model availability

---
+++ booking-accommodation-core/src/main/java/ro/sda/bookingaccommodation/core/entity/Availability.java
@@ -0,0 +1,36 @@
+package ro.sda.bookingaccommodation.core.entity;
+
+import ro.sda.bookingaccommodation.core.base.BaseEntity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "availability", schema = "booking-accommodation")
+public class Availability extends BaseEntity {
+
+    @OneToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+    @Column(name = "room_name", length = 40, nullable = false)
+    private String roomName;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "from_date", length = 8, nullable = false)
+    private Date fromDate;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "to_date", length = 8, nullable = false)
+    private Date toDate;
+
+    @Column (name = "room_type", length = 40, nullable = false)
+    private String roomType;
+
+    @Column (name = "price_double", length = 10, nullable = false)
+    private BigDecimal priceDouble;
+
+    @Column (name = "price_single", length = 10, nullable = false)
+    private BigDecimal priceSingle;
+}
