7aa4c862e3392b59ef334c02eba1cb9a679708a9
Apetri George <apetrigeorge@yahoo.com>
2019-04-07 11:32:10 +0300

    repository for availability, booking and host

---
+++ booking-accommodation-core/src/main/java/ro/sda/bookingaccommodation/core/repository/BookingRepository.java
@@ -0,0 +1,6 @@
+package ro.sda.bookingaccommodation.core.repository;
+
+import ro.sda.bookingaccommodation.core.base.EntityRepository;
+
+public interface BookingRepository extends EntityRepository {
+}
