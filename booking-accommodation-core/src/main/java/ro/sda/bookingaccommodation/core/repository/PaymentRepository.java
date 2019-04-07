8847b1373a74524a65cb21603c8a3ab206766b05
DanutzB <danielbabii@yahoo.com>
2019-04-07 11:30:56 +0300

    payment, property, rating repositories

---
+++ booking-accommodation-core/src/main/java/ro/sda/bookingaccommodation/core/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package ro.sda.bookingaccommodation.core.repository;
+
+import org.w3c.dom.Entity;
+import ro.sda.bookingaccommodation.core.base.EntityRepository;
+import ro.sda.bookingaccommodation.core.entity.Payment;
+
+public interface PaymentRepository extends EntityRepository<Payment> {
+
+}
---
+++ booking-accommodation-core/src/main/java/ro/sda/bookingaccommodation/core/repository/RatingRepository.java
@@ -0,0 +1,8 @@
+package ro.sda.bookingaccommodation.core.repository;
+
+import ro.sda.bookingaccommodation.core.base.EntityRepository;
+import ro.sda.bookingaccommodation.core.entity.Rating;
+
+public interface RatingRepository extends EntityRepository<Rating> {
+    
+}
